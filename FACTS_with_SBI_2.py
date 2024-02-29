27bd0ef24521bfd7fa9662f99d5a46c1b8c6772c
Alvince Pongos <alvincepongos@Alvinces-MBP-2.ucsf.edu>
2024-02-29 14:24:49 -0800

    Updated initial param files for more stable behavior. Updated priors to be more narrow

--- FACTS_with_SBI_2.py
+++ FACTS_with_SBI_2.py
@@ -1697,8 +1697,8 @@ def main(num_sim, num_workers, load_and_train):
     # Low - .002
     # import your simulator, define your prior over the parameters
     #prior_mean = 0.002
-    prior_min= [0.0001, 0.002, 0.0001, 1e-4, 1e-8, 1e-8, 75, 75, 90, 30, 0.94, 1]
-    prior_mmax = [0.004, 0.01, 6.0, 2.0, 1e-4, 1e-4, 250, 250, 200, 200, 1.0, 15]
+    prior_min= [0.0001, 0.0002, 0.01, 1e-8, 1e-8, 1e-8, 75, 75, 90, 30, 0.94, 1]
+    prior_mmax = [0.004, 0.01, 10.0, 1e-4, 1e-4, 1e-4, 200, 200, 200, 100, 1.0, 15]
     #num_sim = 100000
 
     # prior = torch.distributions.Uniform(torch.as_tensor(mmin), torch.as_tensor(mmax) )
